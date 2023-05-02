970661cc7278e44508760ecbb5034ea197e50943
Jules <35456614+expl0itabl3@users.noreply.github.com>
2023-05-02 20:56:35 +0200

    Update nessus_merger.py

--- nessus_merger.py
+++ nessus_merger.py
@@ -1,38 +1,55 @@
 #!/usr/bin/env python3
 
-import xml.etree.ElementTree as Et
 import os
+import xml.etree.ElementTree as Et
 
-output_file = "merged.nessus"
-mainTree = None
-first = True
+# Pattern to match Nessus files
+NESSUS_FILE_PATTERN = ".nessus"
 
-nessus_files = [file for file in os.listdir(".") if ".nessus" in file]
+# Name of the output file
+OUTPUT_FILE_NAME = "merged.nessus"
 
+# Find all Nessus files in the current directory
+nessus_files = [file for file in os.listdir(".") if NESSUS_FILE_PATTERN in file]
+
+# If no Nessus files are found, abort the script
 if not nessus_files:
     print("No Nessus files found. Aborting.")
     exit()
 
-if os.path.exists(output_file):
-    print(f"Output file {output_file} already exists. Aborting.")
+# If the output file already exists, abort the script
+if os.path.exists(OUTPUT_FILE_NAME):
+    print(f"Output file {OUTPUT_FILE_NAME} already exists. Aborting.")
     exit()
 
+# Parse each Nessus file and merge them into a single tree
+main_tree = None
+is_first = True
 for file in nessus_files:
     try:
         with open(file, "r") as f:
             tree = Et.parse(f)
-            if first:
-                mainTree = tree
-                report = mainTree.find("Report")
-                first = False
+            if is_first:
+                # If this is the first file, use its tree as the main tree
+                main_tree = tree
+                report = main_tree.find("Report")
+                is_first = False
             else:
+                # Otherwise, append the ReportHost elements to the main tree's Report element
                 for element in tree.findall(".//ReportHost"):
                     report.append(element)
     except Exception as e:
+        # If there is an error parsing a file, print an error message
         print(f"Error parsing {file}: {str(e)}")
 
-if mainTree is not None:
-    with open(output_file, "wb") as f:
-        mainTree.write(f, encoding="utf-8", xml_declaration=True)
+# If there is a main tree, write it to the output file
+if main_tree is not None:
+    try:
+        with open(OUTPUT_FILE_NAME, "wb") as f:
+            main_tree.write(f, encoding="utf-8", xml_declaration=True)
+    except Exception as e:
+        # If there is an error writing the output file, print an error message
+        print(f"Error writing {OUTPUT_FILE_NAME}: {str(e)}")
 
-print(f"Done. Check your directory for {output_file}!")
+# Finished message
+print(f"Done. Check your directory for {OUTPUT_FILE_NAME}!")
